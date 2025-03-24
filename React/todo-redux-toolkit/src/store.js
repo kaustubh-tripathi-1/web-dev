190665fe1bab5016688f36b5bd455856a6ee9040
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-24 22:49:04 +0530

    Update comment

--- React/todo-redux-toolkit/src/store.js
+++ React/todo-redux-toolkit/src/store.js
@@ -11,8 +11,8 @@ export const store = configureStore({
  * The function `saveStateToLocalStorage` saves the `state.todos` object to the browser's localStorage
  * after serializing it to JSON format.
  * @param state - The `state` parameter in the `saveStateToLocalStorage` function likely represents the
- * current state of an application or a specific part of it. In this case, it seems to be related to a
- * todo list application since it is saving the `state.todos` to the local storage. The `state
+ * current state of an application or a specific part of it. In this case, it seems to be related to a todo
+ * list application since it is saving the `state.todos` to the local storage. The `state
  */
 function saveStateToLocalStorage(state) {
     try {
