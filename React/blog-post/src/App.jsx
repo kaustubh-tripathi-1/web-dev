efed722a4c657e4f07cb82ac23cf0a22ff6bbf7b
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-25 07:49:53 +0530

    Check if tinymce is working

--- React/blog-post/src/App.jsx
+++ React/blog-post/src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import BlogEditor from "./BlogEditor";
 
 export default function App() {
+    const [content, setContent] = useState("");
     return (
         <>
-            <h1>Hi</h1>
+            <div>
+                <BlogEditor onContentChange={setContent} />
+                <div>
+                    <h2>Preview:</h2>
+                    <div dangerouslySetInnerHTML={{ __html: content }} />
+                </div>
+            </div>
         </>
     );
 }
