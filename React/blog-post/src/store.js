f3532ba0b949d1490c0641e6c7c49ee57f4ad913
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-03-31 10:26:59 +0530

    Add uiReducer

--- React/blog-post/src/store.js
+++ React/blog-post/src/store.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import postsReducer from "./slices/postsSlice";
+import postEditorReducer from "./slices/postEditorSlice";
+import storageReducer from "./slices/storageSlice";
+import uiReducer from "./slices/uiSlice";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         posts: postsReducer,
+        postsEditor: postEditorReducer,
+        storage: storageReducer,
+        ui: uiReducer,
     },
 });
