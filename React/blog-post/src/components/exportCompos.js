8eddc5a6f98b5a0af375b98e74884a9f999b46dd
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-02 03:53:02 +0530

    import all compos here and export

---
+++ React/blog-post/src/components/exportCompos.js
@@ -0,0 +1,9 @@
+import Header from "./header/Header.jsx";
+import Home from "./home/Home.jsx";
+import Layout from "./layout/Layout.jsx";
+import Footer from "./footer/Footer.jsx";
+import Login from "./login/Login.jsx";
+import Signup from "./signup/Singup.jsx";
+import BlogEditor from "./text-editor/BlogEditor.jsx";
+
+export { Header, Home, Layout, Footer, Login, Signup, BlogEditor };
