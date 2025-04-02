01441694ef24bf171a6b68fbb8e7e925bd8729f8
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-02 06:45:15 +0530

    Add a Protected compo to render children only when logged in

---
+++ React/blog-post/src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+
+export default function ProtectedRoute({ children }) {
+    const { authStatus } = useSelector((state) => state.auth);
+    if (!authStatus) {
+        return <Navigate to="/login" replace />;
+    }
+    return children;
+}
