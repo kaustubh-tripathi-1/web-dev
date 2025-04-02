efc9c5a7539dc1d818c6d2cf7e6143942b21ee21
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-03 00:53:27 +0530

    Load theme preference ( state ) from localStorage

--- React/blog-post/src/components/layout/Layout.jsx
+++ React/blog-post/src/components/layout/Layout.jsx
@@ -1,14 +1,29 @@
 import { Outlet } from "react-router";
 import { Footer, Header } from "../exportCompos";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export default function Layout() {
+    const { theme, isModalOpen } = useSelector((state) => state.ui);
+
+    // Toggle the 'dark' class on the html element based on the theme
+    useEffect(() => {
+        const html = document.documentElement;
+        if (theme === "dark") {
+            html.classList.add("dark");
+        } else {
+            html.classList.remove("dark");
+        }
+    }, [theme]);
+
     return (
-        <div>
+        <>
             <Header />
             <main>
                 <Outlet />
             </main>
+            {/* {isModalOpen && <Modal />} */}
             <Footer />
-        </div>
+        </>
     );
 }
