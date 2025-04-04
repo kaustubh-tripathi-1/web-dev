5d5e7c559f58b63dd8a11fa1df96bc5b17489892
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-05 05:26:19 +0530

    Add spinner compo

--- React/blog-post/src/components/exportCompos.js
+++ React/blog-post/src/components/exportCompos.js
@@ -4,10 +4,11 @@ import Layout from "./layout/Layout.jsx";
 import Footer from "./footer/Footer.jsx";
 import Login from "./login/Login.jsx";
 import Signup from "./signup/Singup.jsx";
-import BlogEditor from "./text-editor/BlogEditor.jsx";
+import BlogEditor from "./blog-editor/BlogEditor.jsx";
 import About from "./about/About.jsx";
 import Contact from "./contact/Contact.jsx";
 import HomeSkeleton from "./skeletons/HomeSkeleton.jsx";
+import Spinner from "./spinner/Spinner.jsx";
 
 export {
     Header,
@@ -20,4 +21,5 @@ export {
     About,
     Contact,
     HomeSkeleton,
+    Spinner,
 };
