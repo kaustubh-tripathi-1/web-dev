dfbda0d810dc4e1f91f25723b287be2bf5239950
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-07 04:55:41 +0530

    Add an AuthWrapper and use initialLoading state to show spinner

--- React/blog-post/src/App.jsx
+++ React/blog-post/src/App.jsx
@@ -1,6 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Layout from "./components/layout/Layout";
-import { Home, Login, Signup, About, Contact } from "./components/exportCompos";
+import {
+    Home,
+    Login,
+    Signup,
+    About,
+    Contact,
+    Spinner,
+} from "./components/exportCompos";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { checkAuthStatus } from "./slices/authSlice";
 
 const router = createBrowserRouter([
     {
@@ -42,6 +52,29 @@ const router = createBrowserRouter([
     },
 ]);
 
+function AuthWrapper({ children }) {
+    const dispatch = useDispatch();
+    const { initialLoading } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+        dispatch(checkAuthStatus());
+    }, [dispatch]);
+
+    if (initialLoading) {
+        return (
+            <section className="min-h-screen flex justify-center items-center bg-white text-gray-900 dark:bg-gray-800 dark:text-white">
+                <Spinner size="4" />
+            </section>
+        );
+    }
+
+    return children;
+}
+
 export default function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <AuthWrapper>
+            <RouterProvider router={router} />
+        </AuthWrapper>
+    );
 }
