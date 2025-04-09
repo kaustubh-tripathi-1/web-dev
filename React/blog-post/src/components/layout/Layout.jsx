394f8c5bc22e3b28072fb0c31963931af5cdc4ce
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-09 10:27:24 +0530

    Move theme logic to App

--- React/blog-post/src/components/layout/Layout.jsx
+++ React/blog-post/src/components/layout/Layout.jsx
@@ -1,33 +1,19 @@
-import { Outlet, useLocation } from "react-router";
+import { Outlet } from "react-router";
 import { Footer, Header } from "../exportCompos";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 export default function Layout() {
-    const { theme, isModalOpen } = useSelector((state) => state.ui);
-
-    const location = useLocation();
-    const hideHeaderFooter =
-        location.pathname === "/login" || location.pathname === "/signup";
-
-    // Toggle the 'dark' class on the html element based on the theme
-    useEffect(() => {
-        const html = document.documentElement;
-        if (theme === "dark") {
-            html.classList.add("dark");
-        } else {
-            html.classList.remove("dark");
-        }
-    }, [theme]);
+    const { isModalOpen } = useSelector((state) => state.ui);
 
     return (
         <div className="flex flex-col min-h-screen">
-            {!hideHeaderFooter && <Header />}
+            <Header />
             <main className="flex-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-200">
                 <Outlet />
             </main>
             {/* {isModalOpen && <Modal />} */}
-            {!hideHeaderFooter && <Footer />}
+            <Footer />
         </div>
     );
 }
