bb2b6d7d86a93d0e32487413ed4cc9ec3d576b55
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-10 17:21:22 +0530

    Wrap the app with ErrorBoundary

--- React/blog-post/src/main.jsx
+++ React/blog-post/src/main.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
+import { ErrorBoundary } from "./components/exportCompos.js";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <ErrorBoundary>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </ErrorBoundary>
     </StrictMode>
 );
