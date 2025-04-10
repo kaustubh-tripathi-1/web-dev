dd9cd1cfdc402d16ec5e81adadda50421ac62e77
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-10 12:35:23 +0530

    Rename HomeSkeleton to HomePostCardSkeleton

--- React/blog-post/src/components/skeletons/HomePostCardSkeleton.jsx
+++ React/blog-post/src/components/skeletons/HomePostCardSkeleton.jsx
@@ -1,4 +1,4 @@
-export default function HomeSkeleton() {
+export default function HomePostCardSkeleton() {
     return (
         <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded-lg shadow-md animate-pulse-fast">
             <div className="h-6 bg-gray-300 dark:bg-gray-600 rounded mb-2"></div>
