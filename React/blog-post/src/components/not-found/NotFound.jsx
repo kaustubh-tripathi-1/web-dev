cceb4ecfd62395d5200434b0bdfe8f75d36cb6d3
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-10 17:20:35 +0530

    Add pathname to error and add some animation

--- React/blog-post/src/components/not-found/NotFound.jsx
+++ React/blog-post/src/components/not-found/NotFound.jsx
@@ -1,22 +1,31 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import image404 from "../../assets/not-found-404.jpg";
 
 export default function NotFound() {
+    const location = useLocation();
+
     return (
-        <div className="min-h-screen w-screen bg-white text-red-600 text-2xl dark:bg-gray-800 dark:text-red-400 flex flex-col justify-center items-center">
-            <img src={image404} alt="not-found-404" className="w-80" />
+        <div className="min-h-screen w-full bg-white text-red-600 dark:bg-gray-800 dark:text-red-400 flex flex-col justify-center items-center">
+            <img
+                src={image404}
+                alt="404 not found"
+                className="w-80 shadow-lg shadow-black dark:shadow-gray-50 rounded-2xl"
+            />
             {/* Attribution */}
             <a
                 href="https://www.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_13315300.htm#fromView=keyword&page=1&position=0&uuid=d7de2c0b-baee-45c3-af52-66e080ec7db7&query=404+Page+Found"
                 target="_blank"
-                className="text-sm text-amber-50 -ml-35"
+                className="text-sm text-gray-800 dark:text-gray-50 -ml-35"
+                rel="noopener noreferrer"
             >
                 Image by storyset on Freepik
             </a>
-            <p className="mt-4">This page does not exist</p>
+            <p className="mt-4 text-2xl animate-fade-in" role="alert">
+                Page "{location.pathname}" not found. This page does not exist
+            </p>
             <Link
                 to="/"
-                className="cursor-pointer mt-4 text-blue-500 dark:text-blue-500 hover:text-blue-700 hover:underline focus:text-blue-700 focus:outline-none"
+                className="cursor-pointer mt-4 text-2xl text-blue-500 dark:text-blue-500 hover:text-blue-700 hover:underline focus:text-blue-700 focus:outline-none focus:underline transition-colors duration-200"
             >
                 Return to Home Page
             </Link>
