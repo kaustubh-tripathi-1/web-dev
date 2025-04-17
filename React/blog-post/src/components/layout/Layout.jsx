ec8567090a6e58710ffeda586afc8cd74dc361fb
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-18 03:35:47 +0530

    Remove the wrapper div

--- React/blog-post/src/components/layout/Layout.jsx
+++ React/blog-post/src/components/layout/Layout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router";
 import { Footer, Header, Modal } from "../exportCompos";
 import { useSelector } from "react-redux";
+import { Notifications } from "../exportCompos";
 
 /**
  * Renders the main app layout with header, footer, and child routes.
  * @returns {JSX.Element} The layout structure with Outlet for child routes.
  */
 export default function Layout() {
-    const { isModalOpen } = useSelector((state) => state.ui);
+    const { isModalOpen, notifications } = useSelector((state) => state.ui);
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -15,7 +16,11 @@ export default function Layout() {
             <main className="flex-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-200">
                 <Outlet />
             </main>
-            {isModalOpen && <Modal />}
+            {/* Notifications (conditionally rendered) */}
+            {notifications.length > 0 && <Notifications />}
+            {isModalOpen && (
+                <Modal modalType={modalType} modalData={modalData} />
+            )}
             <Footer />
         </div>
     );
