7e5c1321277324a61282fe15778d8fa09bfa9252
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-04-20 00:06:15 +0530

    Remove Notifications and subscribe to modalType and modalData states

--- React/blog-post/src/components/layout/Layout.jsx
+++ React/blog-post/src/components/layout/Layout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router";
 import { Footer, Header, Modal } from "../exportCompos";
 import { useSelector } from "react-redux";
-import { Notifications } from "../exportCompos";
 
 /**
  * Renders the main app layout with header, footer, and child routes.
  * @returns {JSX.Element} The layout structure with Outlet for child routes.
  */
 export default function Layout() {
-    const { isModalOpen, notifications } = useSelector((state) => state.ui);
+    const { isModalOpen, modalType, modalData } = useSelector(
+        (state) => state.ui
+    );
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -16,8 +17,6 @@ export default function Layout() {
             <main className="flex-1 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-200">
                 <Outlet />
             </main>
-            {/* Notifications (conditionally rendered) */}
-            {notifications.length > 0 && <Notifications />}
             {isModalOpen && (
                 <Modal modalType={modalType} modalData={modalData} />
             )}
