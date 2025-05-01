8b9392a8386f619da088602577240d9124b2a2ad
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-02 05:04:16 +0530

    Update resend route to "resend-verification-email"

--- React/blog-post/src/components/verify-email/VerifyEmail.jsx
+++ React/blog-post/src/components/verify-email/VerifyEmail.jsx
@@ -117,7 +117,7 @@ export default function VerifyEmail() {
                             </p>
                             <button
                                 onClick={() => {
-                                    navigate(`/resend-email`);
+                                    navigate(`/resend-verification-email`);
                                 }}
                                 className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
                             >
