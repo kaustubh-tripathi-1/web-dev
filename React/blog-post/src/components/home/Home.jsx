a854178016cce924fca1db588389a294bd7439ee
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-04 03:27:01 +0530

    Update Hero section heading

--- React/blog-post/src/components/home/Home.jsx
+++ React/blog-post/src/components/home/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
             {!authStatus && (
                 <section className="flex flex-col items-center justify-center py-12 px-4 mb-4 bg-gray-100 dark:bg-gray-900 rounded-xl">
                     <h1 className="text-center text-4xl font-bold mb-4">
-                        Welcome to Blogsmith 🛠️
+                        Welcome to BlogSmith 🛠️
                     </h1>
                     <p className="text-lg text-center text-gray-600 dark:text-gray-300">
                         A modern platform to write and share blog posts
