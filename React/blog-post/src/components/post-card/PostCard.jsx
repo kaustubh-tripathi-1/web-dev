248a400788aae39c4ed4ca00941414b373d1685e
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-04 05:41:04 +0530

    Remove outline from PostCard when focused

--- React/blog-post/src/components/post-card/PostCard.jsx
+++ React/blog-post/src/components/post-card/PostCard.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router";
 
 function PostCard({ to, title }) {
     return (
-        <NavLink to={to}>
+        <NavLink to={to} className="outline-none">
             <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded-lg shadow-md">
                 <h2 className="text-xl font-semibold mb-4 hover:text-blue-300 dark:hover:text-blue-300">
                     {title}
