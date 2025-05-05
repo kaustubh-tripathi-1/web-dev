31c091081b5e130263b7176748e1940296327b6e
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-05 10:11:05 +0530

    Add search svg

--- React/blog-post/src/components/search/SearchModalContent.jsx
+++ React/blog-post/src/components/search/SearchModalContent.jsx
@@ -70,18 +70,37 @@ export default function SearchModalContent() {
 
     return (
         <div className="flex flex-col gap-4 animate-fade-in">
-            <form onSubmit={handleSubmit(searchOnSubmit)} className="relative">
+            <form
+                onSubmit={handleSubmit(searchOnSubmit)}
+                className="relative flex justify-center items-center"
+            >
+                <label htmlFor="search">
+                    <svg
+                        className="w-5 h-5 absolute top-1/2 transform -translate-y-1/2 translate-x-1/2"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                    </svg>
+                </label>
                 <input
+                    id="search"
                     type="text"
                     {...register("searchQuery")}
-                    // ref={inputRef}
                     ref={(element) => {
                         register("searchQuery").ref(element);
                         inputRef.current = element;
                     }}
                     placeholder="Search posts by title or content..."
                     aria-label="Search posts by title or content"
-                    className="w-full px-4 py-2 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300"
+                    className="w-full pl-10 pr-4 py-2 text-gray-900 dark:text-gray-100 bg-gray-100 dark:bg-gray-700 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all duration-300"
                 />
                 {searchQuery && (
                     <button
