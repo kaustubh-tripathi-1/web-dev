3bf0defb9dab89f64785d343eac7de60189ecbe3
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-05 08:03:33 +0530

    Close modal when PostCard is clicked

--- React/blog-post/src/components/post-card/PostCard.jsx
+++ React/blog-post/src/components/post-card/PostCard.jsx
@@ -1,11 +1,22 @@
 import { memo } from "react";
 import { NavLink } from "react-router";
 
-function PostCard({ to, title }) {
+function PostCard({ to, title, onClick }) {
+    function handleClick(e) {
+        if (onClick) {
+            onClick(e);
+        }
+    }
+
     return (
-        <NavLink to={to} className="outline-none no-underline">
-            <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded-lg shadow-md">
-                <h2 className="text-xl font-semibold mb-4 hover:text-blue-300 dark:hover:text-blue-300">
+        <NavLink
+            to={to}
+            tabIndex={0}
+            className="block focus:outline-1 focus:outline-blue-500 focus:outline-offset-2 no-underline"
+            onClick={handleClick}
+        >
+            <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded-lg shadow-md hover:shadow-xl transition-all duration-300">
+                <h2 className="text-xl font-semibold mb-4 hover:text-blue-300 dark:hover:text-blue-300 mt-0">
                     {title}
                 </h2>
                 <span className="text-blue-500 dark:text-blue-400 hover:underline focus:underline focus:outline-none">
