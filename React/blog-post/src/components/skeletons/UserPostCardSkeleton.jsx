a42d2bc4b287cf623b1c02a1c8d2029e4c25966f
kaustubh-tripathi-1 <kt40060@gmail.com>
2025-05-06 05:39:30 +0530

    Add skeleton for  user posts loading state

---
+++ React/blog-post/src/components/skeletons/UserPostCardSkeleton.jsx
@@ -0,0 +1,8 @@
+export default function UserPostCardSkeleton() {
+    return (
+        <div className="p-4 bg-gray-100 dark:bg-gray-700 rounded-lg shadow-md animate-pulse-fast">
+            <div className="h-6 bg-gray-300 dark:bg-gray-600 rounded mb-6"></div>
+            <div className="h-4 bg-gray-300 dark:bg-gray-600 rounded w-2/4"></div>
+        </div>
+    );
+}
